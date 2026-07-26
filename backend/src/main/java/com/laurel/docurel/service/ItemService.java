d77c7f75f9a5e3963dc094e207e23e6f307a262c
[Laurel] <[le.nh4026@gmail.com]>
2026-07-26 15:06:31 +0700

    refactor: refactor service layer for DB integration, and changed file storage strategy

--- backend/src/main/java/com/laurel/docurel/service/ItemService.java
+++ backend/src/main/java/com/laurel/docurel/service/ItemService.java
@@ -3,92 +3,127 @@ package com.laurel.docurel.service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.laurel.docurel.model.Document;
-import com.laurel.docurel.model.Folder;
+import com.laurel.docurel.dto.FolderResponse;
+import com.laurel.docurel.dto.ItemResponse;
+import com.laurel.docurel.entity.ItemEntity;
+import com.laurel.docurel.enums.ItemType;
+import com.laurel.docurel.repository.ItemRepository;
+
+import jakarta.transaction.Transactional;
 
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.UUID;
 
 @Service
+@Transactional
 public class ItemService {
-    private static Folder scrapeDirectory(Path loc) {
-        Folder root = new Folder(loc.getFileName().toString());
-        try (DirectoryStream<Path> dirstream = Files.newDirectoryStream(loc)) {
-            for (Path path : dirstream) {
-                if (Files.isRegularFile(path)) {
-                    Document document = new Document(Files.size(path), path.getFileName().toString());
-                    root.getChildren().add(document);
-                }
-                else if (Files.isDirectory(path)) {
-                    Folder folder = scrapeDirectory(path);
-                    root.getChildren().add(folder);
-                }
-            } 
-            root.getChildren().sort(Comparator.comparing(item -> !item.getType().equals("folder")));
-            return root;
-        } catch (IOException e) { return new Folder(""); }
+    private static final String STORAGE_PATH = "C:\\CS\\docurel\\storage\\";
+
+    private final ItemRepository itemRepository;
+
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
     }
 
-    public Folder getRoot() {
-        Path loc = Path.of("C:\\CS\\docurel\\testUploads");
-        return scrapeDirectory(loc);
-    }
+    public FolderResponse getRoot() {
+        List<ItemEntity> itemEntities = itemRepository.findAll(); // 1 sql query
+        Map<Long, ItemResponse> itemResponses = new HashMap<>(itemEntities.size());
 
-    public Document storeDocument(MultipartFile document, String destDir) {
-        try {
-            String filename = document.getOriginalFilename();
-            if (filename == null) {throw new IOException("Filename is null");}
+        for (ItemEntity entity : itemEntities) {
+            itemResponses.put(
+                entity.getId(), 
+                entity.getType() == ItemType.DOCUMENT ? new ItemResponse(entity) : new FolderResponse(entity)
+            );
+        }
 
-            // Objects.requireNonNull() throws an exception at runtime if null
-            Path dest = Objects.requireNonNull(Path.of("C:\\CS\\docurel\\" + destDir + "\\", filename));
-            if (Files.exists(dest)) return null;
-            document.transferTo(dest);
-
-            return new Document(document.getSize(), document.getOriginalFilename());
-        } catch (IOException e) { return null; }
-    }
-
-    public byte[] getFileBytes(String filename) {
-        Path loc = Path.of("C:\\CS\\docurel\\testUploads\\" + filename);
-        try {
-            return Files.readAllBytes(loc);
-        } catch (IOException e) {return null;} 
-    }
-
-    public Folder deleteDocument(String filename) {
-        Path loc = Path.of("C:\\CS\\docurel\\testUploads\\" + filename);
-        try {
-            Files.delete(loc);
-        } catch (IOException e) {} 
-        return getRoot();
-    }
-
-    public Folder createDirectory(String foldername) {
-        Path loc = Path.of("C:\\CS\\docurel\\testUploads\\" + foldername);
-        try {
-            Files.createDirectory(loc);
-            return new Folder(foldername);
-        } catch (IOException e) { return null; }
-    }
-
-    public Folder deleteDirectory(String foldername) {
-        Path loc = Path.of("C:\\CS\\docurel\\testUploads\\" + foldername);
-        deleteDirectoryRecursive(loc);
-        return getRoot();
-    }
-
-    private static void deleteDirectoryRecursive(Path loc) {
-        try {
-            if (Files.isDirectory(loc)) {
-                try (DirectoryStream<Path> dirstream = Files.newDirectoryStream(loc)) {   
-                    for (Path path : dirstream) deleteDirectoryRecursive(path);        
-                }
+        FolderResponse rootResponse = null;
+        for (ItemEntity entity : itemEntities) { // iterate over Entity list again since Responses dont store parentId
+            Long id = entity.getId();
+            Long parentId = entity.getParentId();
+            if (parentId == null) {
+                rootResponse = (FolderResponse) itemResponses.get(id);
+                continue;
             }
-            Files.delete(loc);
-        } catch (Exception e) {e.printStackTrace();} 
+            FolderResponse parentResponse = (FolderResponse) itemResponses.get(parentId);
+            parentResponse.getChildren().add(itemResponses.get(id));
+        }
+        return rootResponse;
     }
+
+    @SuppressWarnings("null")
+    public ItemResponse storeDocument(MultipartFile document, UUID publicParentId) throws IOException {
+        Long parentId = itemRepository.findIdByPublicId(publicParentId);
+        String filename = Objects.requireNonNull(document.getOriginalFilename());
+        
+        if (itemRepository.existsByParentIdAndName(parentId, filename))  {
+            throw new IllegalArgumentException("An item with that name already exists in that location");
+        }
+        ItemEntity entity = new ItemEntity(filename, parentId, ItemType.DOCUMENT, document.getSize());
+        itemRepository.save(entity);
+
+        Path dest = Path.of(STORAGE_PATH, entity.getId().toString());            
+        document.transferTo(dest);
+
+        return new ItemResponse(entity);
+    }
+
+    public byte[] getFileBytes(UUID publicId) throws IOException {
+        Long id = itemRepository.findIdByPublicId(publicId);
+        Path loc = Path.of(STORAGE_PATH, id.toString());
+        return Files.readAllBytes(loc); 
+    }
+
+    public FolderResponse deleteDocument(UUID publicId) throws IOException {
+        Long id = itemRepository.findIdByPublicId(publicId);
+        Path loc = Path.of(STORAGE_PATH, id.toString());
+        Files.delete(loc);
+
+        itemRepository.deleteById(id); 
+        return getRoot();
+    }
+
+    // only create a logical Folder entry on the DB
+    public FolderResponse createDirectory(String foldername, UUID publicParentId) {
+        Long parentId = itemRepository.findIdByPublicId(publicParentId);
+        if (itemRepository.existsByParentIdAndName(parentId, foldername))  {
+            throw new IllegalArgumentException("An item with that name already exists in that location");
+        }
+
+        ItemEntity folderEntity = itemRepository.save(new ItemEntity(foldername, parentId, ItemType.FOLDER, null));
+        return new FolderResponse(folderEntity);
+    }
+
+    @SuppressWarnings("null")
+    public FolderResponse deleteDirectory(UUID publicId) throws IOException {
+        Long rootId = itemRepository.findIdByPublicId(publicId);
+        List<Long> toDeleteIds = itemRepository.findDocumentIdsByTree(rootId);
+        for (Long id : toDeleteIds) {
+            Path loc = Path.of(STORAGE_PATH, id.toString());
+            Files.delete(loc);
+        }
+        itemRepository.deleteById(rootId);
+        return getRoot();
+    }
+
+    // @SuppressWarnings("null")
+    // // + throw proper exception from .orElseThrow();
+    // private FolderResponse buildFolderTree(Long id) {
+    //     try {
+    //         ItemEntity folderEntity = itemRepository.findById(id).orElseThrow();    
+    //         FolderResponse folderResponse = new FolderResponse(folderEntity);
+
+    //         List<ItemEntity> children = itemRepository.findByParentId(id);
+    //         for (ItemEntity child : children) {
+    //             if (child.getType() == ItemType.FOLDER) folderResponse.getChildren().add(buildFolderTree(child.getId()));
+    //             else folderResponse.getChildren().add(new ItemResponse(child));
+    //         }
+    //         return folderResponse;
+    //     } catch (Exception e) { return null; }
+    // }
 }

528ebdec65a5d9f46a142550b75a07947d004cb1
[Laurel] <[le.nh4026@gmail.com]>
2026-08-04 11:07:42 +0700

    feat: modal overlay for folder upload

--- frontend/src/components/FolderUpload.jsx
+++ frontend/src/components/FolderUpload.jsx
@@ -1,11 +1,13 @@
+import "./css/FolderUpload.css"
 import { useState } from "react";
 import { useExplorer } from "../ExplorerContext";
 
 function FolderUpload() {
   const {createFolder} = useExplorer();
 
+  const [creatingFolder, setCreatingFolder] = useState(false);
   const [foldernameToCreate, setFoldernameToCreate] = useState("");
-
+  
   function uploadFolderToCreate() {
     if (!foldernameToCreate || foldernameToCreate === "") return;
     createFolder(foldernameToCreate);
@@ -14,16 +16,42 @@ function FolderUpload() {
 
   return (
     <div>
-      <input
-        type="text"
-        value={foldernameToCreate}
-        placeholder="Enter Folder name here"
-        onChange={(e) => setFoldernameToCreate(e.target.value.trim())}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") uploadFolderToCreate();
+      {
+        creatingFolder &&
+        <div className="overlay" onClick={(e) => {
+          e.stopPropagation();
+          setCreatingFolder(false);
+        }}>
+          <div className="create-folder-modal" onClick={(e) => {e.stopPropagation()}}>
+            <div> Enter Folder name </div>
+            <input
+              type="text"
+              value={foldernameToCreate}
+              placeholder="Enter Folder name here"
+              onChange={(e) => setFoldernameToCreate(e.target.value.trim())}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") uploadFolderToCreate();
+              }}
+            />
+            <div className="modal-buttons">
+              <button onClick={() => {setCreatingFolder(false)}}> Cancel </button>
+              <button onClick={() => {
+                uploadFolderToCreate();
+                setCreatingFolder(false);
+              }}> 
+                Confirm 
+              </button>
+            </div>
+          </div>
+        </div>
+      }
+      <button 
+        className="create-folder-button"
+        onClick={(e) => {
+          e.stopPropagation();
+          setCreatingFolder(true);
         }}
-      />
-      <button onClick={uploadFolderToCreate}>
+      >
         Create Folder
       </button>
     </div>

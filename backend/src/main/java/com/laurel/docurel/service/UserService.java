f9b68624d031ae723faa809e143fb188e075d060
[Laurel] <[le.nh4026@gmail.com]>
2026-08-04 08:35:06 +0700

    fix: remove circular dependency between UserService and ItemService

--- backend/src/main/java/com/laurel/docurel/service/UserService.java
+++ backend/src/main/java/com/laurel/docurel/service/UserService.java
@@ -5,24 +5,32 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.laurel.docurel.dto.response.LoginResponse;
+import com.laurel.docurel.entity.ItemEntity;
 import com.laurel.docurel.entity.UserEntity;
-import com.laurel.docurel.exception.InvalidPermissionsException;
+import com.laurel.docurel.entity.UserItemEntity;
+import com.laurel.docurel.enums.ItemType;
+import com.laurel.docurel.enums.PermissionType;
+import com.laurel.docurel.repository.ItemRepository;
+import com.laurel.docurel.repository.UserItemRepository;
 import com.laurel.docurel.repository.UserRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional // so failed query pipelines can be rolled back
 public class UserService {
     private static final Long GLOBAL_ROOT_ID = 0L;
 
     private final PasswordEncoder passwordEncoder;
+    private final UserItemRepository userItemRepository;
     private final UserRepository userRepository;
+    private final ItemRepository itemRepository;
     private final JwtService jwtService;
-    private final ItemService itemService;
 
     public LoginResponse registerUser(String username, String email, String password) {
-        UserEntity entity = new UserEntity(username, email, passwordEncoder.encode(password));
+        UserEntity user = new UserEntity(username, email, passwordEncoder.encode(password));
 
         if (username != null && userRepository.existsByUsername(username)) {
             throw new IllegalArgumentException("Username is taken");
@@ -30,12 +38,17 @@ public class UserService {
         if (email != null && userRepository.existsByEmail(email)) {
             throw new IllegalArgumentException("Email is taken");
         }
+        user = userRepository.save(user);
 
-        entity = userRepository.save(entity);
         String identifier = (username == null ? email : username);
-        try { itemService.createDirectory(identifier, itemService.getItemPublicId(GLOBAL_ROOT_ID)); }
-        catch (InvalidPermissionsException e) { /* creation of user root in global root is allowed upon registration */}
-        return new LoginResponse(entity, jwtService.generateToken(entity));
+
+        // above already checks duplicate folder name (identifier can't be duplicate since username and email can't)
+        ItemEntity userRootFolder = itemRepository.save(new ItemEntity(identifier, GLOBAL_ROOT_ID, ItemType.FOLDER, null));
+        // can't use getCurrentUser, since if user is trying to register, they probably don't have a JWT token yet, 
+        // meaning the request falls past JwtAuthenticationFilter, and no UserEntity would have been loaded into SecurityContextHolder
+        userItemRepository.save(new UserItemEntity(user, userRootFolder, PermissionType.OWNER));
+
+        return new LoginResponse(user, jwtService.generateToken(user));
     }
 
     public LoginResponse loginUser(String usernameOrEmail, String password) {

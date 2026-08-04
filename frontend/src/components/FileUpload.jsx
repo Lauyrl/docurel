e64c329914170e1c5fa133589038af3db324a503
[Laurel] <[le.nh4026@gmail.com]>
2026-08-04 11:08:46 +0700

    refactor: hide default input element, expose a button instead

--- frontend/src/components/FileUpload.jsx
+++ frontend/src/components/FileUpload.jsx
@@ -1,13 +1,17 @@
+import "./css/FileUpload.css"
 import { useExplorer } from "../ExplorerContext";
+import { useRef } from "react";
 
 function FileUpload() {
   const {uploadDocument} = useExplorer();
+  const fileInputRef = useRef(null);
 
   return (
     <div>
       <input
-        /* creates an <input type = "file"> element with default value = "No file selected", creates a built in input UI */
-        type="file"
+        ref={fileInputRef} // lets fileInputRef.current to reference this
+        type="file" // creates an <input type = "file"> element with default value = "No file selected", creates a built in input UI
+        style={{ display: "none" }} // hide the element
         /* e: the event object, is created when <input type = "file"> changes 
            e.target: HTML element that created the event (<input type = "file">)
            e.target.files: access the file property of <input type = "file">
@@ -15,9 +19,16 @@ function FileUpload() {
         onChange={(e) => {
           const file = e.target.files[0];
           if (file) uploadDocument(file);
-          e.target.value = "";
+          e.target.value = ""; // reset this because e.target.files is immutable, resetting lets onChange trigger more reliably later
         }}
       />
+
+      <button
+        className="upload-button"
+        onClick={() => fileInputRef.current.click()} // since the <input/> is hidden, simulate the click through the Ref
+      >
+        Upload File
+      </button>
     </div>
   );
 }

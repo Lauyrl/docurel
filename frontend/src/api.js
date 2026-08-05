f91f54bfa0e886f6493c4afc3a899be3bf919f55
[Laurel] <[le.nh4026@gmail.com]>
2026-08-05 10:27:10 +0700

    refactor: let api() throw exceptions

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,13 +1,17 @@
 const BASE_URL = "http://localhost:8080";
 
-export function api(endpoint, options = {}) {
+export async function api(endpoint, options = {}) {
     const token = localStorage.getItem("jwt");
 
-    return fetch(BASE_URL + endpoint, {
+    const response = await fetch(BASE_URL + endpoint, {
         ...options, // stuff like "method", "body",... 
         headers: {
             ...options.headers,
             ...(token && {"Authorization": "Bearer " + token})
         }
     })
+
+    if (!response.ok) throw new Error(await response.text()); // must await response body as it is async
+
+    return response;
 }

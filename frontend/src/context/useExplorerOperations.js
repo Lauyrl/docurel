c36b5ef15b3ab97e691876a24347cb76d48dab89
[Laurel] <[le.nh4026@gmail.com]>
2026-08-10 19:42:25 +0700

    refactor: merge 2 page-exclusive operations explorers into 1 as they are mostly identical

--- frontend/src/context/useExplorerOperations.js
+++ frontend/src/context/useExplorerOperations.js
@@ -1,4 +1,3 @@
-import { useCallback } from "react";
 import { createFolderCommon, deleteItemCommon, deleteUserPermissionsForItemCommon, editUserPermissionsForItemCommon, getUsersWithPermissionsForItemCommon, patchItemCommon, searchItemsCommon, uploadDocumentCommon } from "../common";
 import { useExplorer } from "./ExplorerContext";
 
@@ -6,8 +5,8 @@ export function initializeFolderUIState(item) {
   return { ...item, isExpanded: false };
 }
 
-function useMyFilesOperations() {
-  const { itemMap, currentFolderId, childrenIndex, setItemMap, setCurrentFolderId, setPreviewItemId, rebuildNavigationStacks, setFilteredItemIdSet, filterValues } = useExplorer();  
+export default function useExplorerOperations(currentPageIdx) {
+  const { itemMap, currentFolderId, childrenIndex, setItemMap, setCurrentFolderId, setPreviewItemId, rebuildNavigationStacks, setFilteredItemIdSet, filterValues } = useExplorer();
 
   function anyFilterActive() {
     return (filterValues.type || filterValues.contentType || filterValues.createdAfter || filterValues.createdBefore || filterValues.updatedAfter || filterValues.updatedBefore);
@@ -73,7 +72,7 @@ function useMyFilesOperations() {
     descending
   ) {
     const filteredItemsPublicId = await searchItemsCommon(
-      true, 
+      (currentPageIdx === 0), 
       searchQuery, 
       type,
       contentType,
@@ -91,99 +90,5 @@ function useMyFilesOperations() {
     initializeFolderUIState, anyFilterActive, selectItem, uploadDocument, createFolder, deleteItem, patchItem, 
     editUserPermissionsForItem, deleteUserPermissionsForItem, getUsersWithPermissionsForItem,
     searchItems,
-  }
-}
-
-function useSharedOperations() {
-  const { itemMap, currentFolderId, childrenIndex, setItemMap, setCurrentFolderId, setPreviewItemId, rebuildNavigationStacks, setFilteredItemIdSet, filterValues } = useExplorer();
-
-  function anyFilterActive() {
-    return (filterValues.type || filterValues.contentType || filterValues.createdAfter || filterValues.createdBefore || filterValues.updatedAfter || filterValues.updatedBefore);
-  }
-
-  function selectItem(item) {
-    setPreviewItemId(null);
-    if (item.type === "DOCUMENT") setPreviewItemId(item.publicId);
-    if (item.type === "FOLDER") {
-      if (item.publicId !== currentFolderId) rebuildNavigationStacks(item.publicId);
-      
-      const itemTemp = { ...item, isExpanded: !item.isExpanded };
-      setItemMap((current) => new Map(current).set(item.publicId, itemTemp)); // make new map with new entry to avoid mutating state
-      setCurrentFolderId(item.publicId);
-    }
-  }
-
-  async function uploadDocument(file) {
-    let document = await uploadDocumentCommon(file, currentFolderId);
-    setItemMap((current) => new Map(current).set(document.publicId, document)); // implicit return
-  }
-
-  async function createFolder(foldername) {
-    let folder = await createFolderCommon(foldername, currentFolderId);
-    if (folder.type === "FOLDER") folder = initializeFolderUIState(folder);
-    setItemMap((current) => new Map(current).set(folder.publicId, folder));
-  }
-
-  async function deleteItem(item) {
-    const newItemMap = await deleteItemCommon(item, itemMap, childrenIndex);
-    setItemMap(newItemMap);
-    selectItem(newItemMap.get(item.publicParentId)); // avoid itemMap.get() since itemMap could be stale?
-  }
-
-  async function patchItem(item, newName, newParentPublicId) {
-    let patchedItem = await patchItemCommon(item, newName, newParentPublicId);
-    if (patchedItem != null) {
-      setItemMap((current) => new Map(current).set(item.publicId, patchedItem));
-    }
-  }
-
-  async function editUserPermissionsForItem(item, newPermissionsInfo) {
-    return await editUserPermissionsForItemCommon(item, newPermissionsInfo);
-  }
-
-  // useCallback prevents functions from being redefined on every re-render (useMyFilesOperations was re-rendering since it depended on values from useExplorer(), which were re-rendering because states were being updated) 
-  const getUsersWithPermissionsForItem = useCallback(async (item) => {
-    return await getUsersWithPermissionsForItemCommon(item);
-  }, []);
-
-  async function searchItems(
-    searchQuery, 
-    type,
-    contentType,
-    createdAfter,
-    createdBefore,
-    updatedAfter,
-    updatedBefore,
-    sortBy,
-    descending
-  ) {
-    const filteredItemsPublicId = await searchItemsCommon(
-      false, 
-      searchQuery, 
-      type,
-      contentType,
-      createdAfter,
-      createdBefore,
-      updatedAfter,
-      updatedBefore,
-      sortBy,
-      descending
-    ); 
-    setFilteredItemIdSet(new Set(filteredItemsPublicId));
-  }
-
-  return {
-    selectItem, anyFilterActive, uploadDocument, createFolder, deleteItem, patchItem, editUserPermissionsForItem, getUsersWithPermissionsForItem, searchItems
-  }
-}
-
-export default function useExplorerOperations(currentPageIdx) {
-  const myFilesOperations = useMyFilesOperations()
-  const sharedOperations = useSharedOperations()
-
-  switch (currentPageIdx) {
-    case 0: return myFilesOperations;
-    case 1: return sharedOperations;
-    default: throw new Error("Unknown page")
   }
 }
